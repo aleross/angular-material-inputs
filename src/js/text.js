126fe7dea49d6aaf05fc508e34da463b54860ffe
Alex Ross <alex@enplug.com>
2015-12-02 14:51:45 -0800

    Adding materialText directive

---
+++ src/js/text.js
@@ -0,0 +1,14 @@
+/**
+ * @ngdoc directive
+ * @name materialText
+ * @module material-inputs
+ *
+ * @restrict E
+ */
+angular.module('material-inputs').directive('materialText', function () {
+    'use strict';
+
+    return {
+
+    };
+});
---
+++ test/unit/text.js
@@ -0,0 +1,3 @@
+describe('material-text', function () {
+
+});
