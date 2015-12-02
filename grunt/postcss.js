714b173b34441a08a25bedac3ba7807a71360738
Alex Ross <alex@enplug.com>
2015-12-02 13:57:22 -0800

    Add CSS auto-prefixer

---
+++ grunt/postcss.js
@@ -0,0 +1,12 @@
+module.exports = {
+    options: {
+        map: false, // will point to sass task css map
+        processors: [
+            require('pixrem')(), // add fallbacks for rem units
+            require('autoprefixer-core')({ browsers: 'last 2 versions' }) // add vendor prefixes
+        ]
+    },
+    build: {
+        src: 'dist/material-inputs.css'
+    }
+};
