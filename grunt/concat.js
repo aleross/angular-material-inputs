abe72f2aa4b61f47039c156fa52cf8134e73be9f
Alex Ross <alex@enplug.com>
2015-12-06 22:22:22 -0800

    remove html2js and input directive

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,6 @@ module.exports = {
     js: {
         src: [
             'src/js/module.js',
-            'tmp/templates.js',
             'src/js/**/*.js',
         ],
         dest: 'dist/material-inputs.js',
