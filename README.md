32ce7b42a0e9fc0c6c1cebb3dd8ae575e7add4e6
Alex Ross <alex@enplug.com>
2015-12-13 18:21:18 -0800

    Add dist files to git, update README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# angular-material-inputs
-AngularJS directives for material design HTML inputs
+# [Material design](https://www.google.com/design/spec/material-design/introduction.html) form inputs for [AngularJS](https://angular.io/)
+AngularJS directives that make it easy to use material design in your forms.
+
+[![Build Status](https://travis-ci.org/aleross/angular-material-inputs.svg?branch=master)](https://travis-ci.org/aleross/angular-material-inputs)
+[![Coverage Status](https://coveralls.io/repos/aleross/angular-material-inputs/badge.svg?branch=master&service=github)](https://coveralls.io/github/aleross/angular-material-inputs?branch=master)
+[![Dependencies Status](https://david-dm.org/aleross/angular-material-inputs.svg)](https://david-dm.org/aleross/angular-material-inputs.svg)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/aleross/angular-material-inputs/blob/master/LICENSE)
+
+This project is a lightweight implementation of the material design form inputs. 
+If you're looking for a more comprehensive implementations of Google's material design, check out:
+- [Angular Material](https://material.angularjs.org/latest/)
+- [Material Design Lite](http://www.getmdl.io/)
---
+++ dist/material-inputs.js
@@ -0,0 +1,178 @@
+angular.module('material-inputs', []);
+
+angular.module('material-inputs').constant('miClasses', {
+    INPUT: 'mi-input',
+    TEXT: 'mi-text',
+
+    // input states
+    FOCUSED: 'mi-focused',
+    HAS_VALUE: 'mi-has-value',
+
+    // touched?
+    // dirty?
+});
+
+(function (module) {
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @name materialText
+     * @module material-inputs
+     *
+     * @restrict E
+     *
+     * @param mi-label
+     */
+    module.directive('materialText', materialTextDirective);
+    module.directive('input', inputDirective);
+
+    function materialTextDirective($compile, miClasses) {
+
+        /**
+         * The post link function is executed in a bottom-up manner, so the HTML
+         * of child directives is already available. If a child directive has added an input
+         * or a label, this function will see it and not auto-add.
+         * @param scope
+         * @param element
+         * @param attrs
+         */
+        function postLink(scope, element, attrs) {
+            setupInput(scope, element);
+            setupLabel(scope, element, attrs['miLabel']);
+        }
+
+        /**
+         * Adds an input if one hasn't been provided.
+         * @param scope
+         * @param element
+         */
+        function setupInput(scope, element) {
+            var input = element.find('input');
+            if (input.length > 1) {
+                throw new Error('material-text can only have one <input>.');
+            }
+
+            if (!input.length) {
+                input = angular.element('<input>');
+                element.append(input);
+                $compile(input)(scope);
+            }
+        }
+
+        /**
+         * Adds a label if one hasn't been provided.
+         * @param scope
+         * @param element
+         * @param miLabel
+         */
+        function setupLabel(scope, element, miLabel) {
+            var label = element.find('label');
+            if (label.length > 1) {
+                throw new Error('material-text can only have one <label>.');
+            }
+
+            if (!label.length) {
+                var newLabel = angular.element('<label>' + (miLabel || '') + '</label>');
+                element.append(newLabel);
+                $compile(label)(scope);
+            }
+        }
+
+        return {
+
+            compile: function (element) {
+                element.addClass(miClasses.INPUT + ' ' + miClasses.TEXT);
+                return postLink;
+            },
+
+            controller: function ($scope, $element) {
+
+                function toggleClass(bool, name) {
+                    if (bool) $element.addClass(name); else $element.removeClass(name);
+                }
+
+                this.setFocused = function (isFocused) {
+                    toggleClass(isFocused, miClasses.FOCUSED);
+                };
+
+                this.setHasValue = function (hasValue) {
+                    toggleClass(hasValue, miClasses.HAS_VALUE);
+                };
+            }
+        };
+    }
+
+    /**
+     * <input> directive listens to value updates
+     *  and notifies <material-text> via shared controller.
+     *  Does not run for inputs not wrapped in <material-text>.
+     */
+    function inputDirective() {
+        return {
+            restrict: 'E',
+            require: ['^?materialText', '?ngModel'],
+            link: function (scope, element, attr, ctrls) {
+                var miCtrl = ctrls[0],
+                    ngModelCtrl = ctrls[1];
+
+                // Only use custom input directive
+                // if nested within <material-text>
+                if (!miCtrl) return;
+
+                // Checks for input that is invalid based on native Constraints API.
+                function hasValue() {
+                    return element.val().length > 0 || (element[0].validity || {}).badInput;
+                }
+
+                // Update has-value status when reading values from the DOM ($parsers)
+                // and when model updates are pushed to the DOM ($formatters)
+                if (ngModelCtrl) {
+
+                    // Parse updates state when user interacts with DOM by typing in the input.
+                    // If we have an ng-model controller, we tap into the $parser pipeline because
+                    // it lets us leverage the work that Angular's <input> directive does before calling
+                    // the ngModelCtrl $setViewValue. Lots of cross-browser and fringe-cases, like IE9 not
+                    // supporting 'input' event on backspace etc, or non-Latin language composition events.
+
+                    // Note: this will not be called when a value is entered that is considered invalid by native
+                    // Constraints API. So we also tap into 'input' below.
+                    ngModelCtrl.$parsers.push(function (value) {
+                        miCtrl.setHasValue(!ngModelCtrl.$isEmpty(value));
+                        return value;
+                    });
+
+                    // Formatter updates state when $scope value is set
+                    ngModelCtrl.$formatters.push(function (value) {
+                        miCtrl.setHasValue(!ngModelCtrl.$isEmpty(value));
+                        return value;
+                    });
+                } else {
+
+                    // Call this once in case a static value has been set using value="Foo"
+                    miCtrl.setHasValue(hasValue());
+                }
+
+                // We listen to the 'input' event as well as tapping into the $parsers pipeline
+                // in case the user has entered a value into an input, but the native Constraints API
+                // has hidden the value from the $parsers pipeline because it is invalid.
+
+                // Note: there are fringe case and cross-browser issues with listening to only the 'input' event
+                // IE9 backspace, chinese composition event, etc.
+                element.on('input', function () {
+                    miCtrl.setHasValue(hasValue());
+                });
+
+                // Focused state
+                element.on('focus', function () {
+                    miCtrl.setFocused(true);
+                });
+
+                element.on('blur', function () {
+                    miCtrl.setFocused(false);
+                });
+            }
+        }
+    }
+
+}(angular.module('material-inputs')));
