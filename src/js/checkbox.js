b2a48626ddb4aaac5000aa8c5cac712f55664b86
Alex Ross <alex@enplug.com>
2015-12-13 20:26:07 -0800

    <material-checkbox> transclude and unit tests

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -3,20 +3,49 @@
 
     module.directive('materialCheckbox', materialCheckboxDirective);
 
-    function materialCheckboxDirective(miClasses) {
+    function materialCheckboxDirective(miClasses, $compile) {
         return {
             restrict: 'E',
-            template: '<label><input type="checkbox">' +
+            template: '<label>' +
+                '<input type="checkbox">' +
                 '<span class="mi-check"></span>' +
-                '<span class="checkbox-label"><span>Label</span></span>' +
+                '<span class="checkbox-label"></span>' +
             '</label>',
+            transclude: true,
             compile: function (element) {
                 element.addClass(miClasses.INPUT + ' ' + miClasses.CHECKBOX);
-                return function postLink() {
+                return function postLink(scope, element, attrs, ctrl, transclude) {
 
+                    // Copy transcluded input or wrapped html into checkbox label
+                    transclude(function(clone) {
+                        if (clone) {
+
+                            // Check to see if our transcluded HTML includes an input
+                            var label = angular.element(element[0].querySelector('.checkbox-label')),
+                                inputCount = 0;
+
+                            // Add transcluded input or label content in appropriate places
+                            for (var i = 0; i < clone.length; i++) {
+                                if (clone[i].nodeName.toLowerCase() == 'input') {
+                                    element.find('input').remove();
+                                    element.find('label').prepend(clone[i]);
+                                    element.find('input').attr('type', 'checkbox');
+                                    inputCount += 1;
+                                } else {
+                                    label.append(clone[i]);
+                                }
+                            }
+
+                            // Connect our DOM changes to the scope
+                            $compile(element.find('label')[0])(scope);
+
+                            if (inputCount > 1) {
+                                throw new Error('<material-checkbox> can only have one <input>.');
+                            }
+                        }
+                    });
                 }
             }
         }
     }
-
 }(angular.module('material-inputs')));
--- test/unit/checkboxSpec.js
+++ test/unit/checkboxSpec.js
@@ -26,7 +26,37 @@ describe('material-text', function () {
 
     // Class validation
     it('should add the wrapper and checkbox classes', function () {
-        var text = createElement();
-        expect(text).toHaveClass(miClasses.INPUT + ' ' + miClasses.CHECKBOX);
+        var checkbox = createElement();
+        expect(checkbox).toHaveClass(miClasses.INPUT + ' ' + miClasses.CHECKBOX);
+    });
+
+    it('should transclude text as the checkbox label', function () {
+        var test = 'Checkbox',
+            checkbox = createElement(test),
+            labelText = angular.element(checkbox[0].querySelector('.checkbox-label')).text();
+        expect(labelText).toEqual(test);
+    });
+
+    it('should transclude an input as the checkbox input', function () {
+        var test = 'test',
+            checkbox = createElement('<input name="' + test + '">'),
+            inputName = checkbox.find('input').attr('name');
+        expect(inputName).toEqual(test);
+    });
+
+    it('should transclude an input and label text separately', function () {
+        var test = 'test',
+            checkbox = createElement('<label>' + test + '</label><input name="' + test + '">'),
+            inputName = checkbox.find('input').attr('name'),
+            labelText = angular.element(checkbox[0].querySelector('.checkbox-label')).text();
+        expect(inputName).toEqual(test);
+        expect(labelText).toEqual(test);
+    });
+
+    it('should throw an error if more than one input is transcluded', function () {
+        var error = new Error('<material-checkbox> can only have one <input>.');
+        expect(function () {
+            createElement('<input><input>');
+        }).toThrow(error);
     });
 });
